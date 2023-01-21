bd7f66a7b92c3eaaf72250ef0b60f7873cf507f1
EUNSH1 <tkdgus968@naver.com>
2023-01-22 05:23:08 +0900

    feat: ToyProject Basic Frame

---
+++ src/test/java/com/web/toyproject/BeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.toyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
